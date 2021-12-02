f922795eb53e8ed8d81e32d9091a8370d4199f8a
jangjaeko <jangjaeko45@gmail.com>
2021-12-02 17:25:25 +0900

    category edit

--- src/component/SignIn/Category/Category.js
+++ src/component/SignIn/Category/Category.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -8,10 +8,42 @@ import {
 } from 'react-native';
 import Icons from '../../Icons/Icons';
 import CatBut from './Sections/CatBut';
+
 export default function Category(props) {
+  const [isFood, setisFood] = useState(false);
+  const [isMusic, setisMusic] = useState(false);
+  const [isWrite, setisWrite] = useState(false);
+  const [isTravel, setisTravel] = useState(false);
+  const [isPet, setisPet] = useState(false);
+  const [isLove, setisLove] = useState(false);
+  const [isGym, setisGym] = useState(false);
+  const [isGame, setisGame] = useState(false);
+  const [isFashion, setisFashion] = useState(false);
+  const [isCoffee, setisCoffee] = useState(false);
+  const [isBeauti, setisBeauti] = useState(false);
+  const [isbaby, setisbaby] = useState(false);
+  const SendAndGo = () => {
+    var cnt = 0;
+    if (isFood === true) cnt += 1;
+    if (isMusic === true) cnt += 1;
+    if (isWrite === true) cnt += 1;
+    if (isTravel === true) cnt += 1;
+    if (isPet === true) cnt += 1;
+    if (isLove === true) cnt += 1;
+    if (isGym === true) cnt += 1;
+    if (isGame === true) cnt += 1;
+    if (isFashion === true) cnt += 1;
+    if (isCoffee === true) cnt += 1;
+    if (isBeauti === true) cnt += 1;
+    if (isbaby === true) cnt += 1;
+
+    if (isFood) {
+      props.navigation.navigate('Main');
+    }
+  };
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.allView}>
-      <View style={{marginBottom: '100%'}}>
+      <View style={{marginBottom: '70%'}}>
         <TouchableOpacity
           style={{zIndex: 10, marginLeft: '-1%'}}
           onPress={() => props.navigation.goBack()}>
@@ -24,12 +56,39 @@ export default function Category(props) {
         </TouchableOpacity>
         <View style={{marginTop: 40}}>
           <Text style={styles.TitleLetter}>
-            선호하는 카테고리를 {'\n'}3개 선택해주세요
+            선호하는 카테고리를 {'\n'}3개 이상 선택해주세요
           </Text>
         </View>
-
-        <CatBut />
+        <CatBut
+          isFood={isFood}
+          setisFood={setisFood}
+          isBeauti={isBeauti}
+          setisBeauti={setisBeauti}
+          isCoffee={isCoffee}
+          setisCoffee={setisCoffee}
+          isFashion={isFashion}
+          setisFashion={setisFashion}
+          isGame={isGame}
+          setisGame={setisGame}
+          isGym={isGym}
+          setisGym={setisGym}
+          isMusic={isMusic}
+          setisMusic={setisMusic}
+          isTravel={isTravel}
+          setisTravel={setisTravel}
+          isWrite={isWrite}
+          setisWrite={setisWrite}
+          isbaby={isbaby}
+          setisbaby={setisbaby}
+          isLove={isLove}
+          setisLove={setisLove}
+          isPet={isPet}
+          setisPet={setisPet}
+        />
       </View>
+      <TouchableOpacity style={styles.btnDesign} onPress={SendAndGo}>
+        <Text style={styles.btnText}>시작하기</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 }
@@ -42,4 +101,21 @@ const styles = StyleSheet.create({
     paddingLeft: '5%',
   },
   TitleLetter: {fontFamily: 'Roboto-Bold', fontSize: 24, lineHeight: 35},
+  btnDesign: {
+    backgroundColor: '#DEDEDE',
+    width: '95%',
+    height: 50,
+    borderRadius: 10,
+    position: 'absolute',
+
+    top: 1150,
+    justifyContent: 'center',
+  },
+  btnText: {
+    textAlign: 'center',
+    color: 'white',
+    borderRadius: 10,
+    fontFamily: 'NotoSansKR-Medium',
+    fontSize: 18,
+  },
 });
--- src/component/SignIn/Category/Sections/CatBut.js
+++ src/component/SignIn/Category/Sections/CatBut.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import music from '../../../../images/music.png';
 import write from '../../../../images/write.png';
@@ -12,72 +12,296 @@ import fashion from '../../../../images/fashion.png';
 import coffee from '../../../../images/coffee.png';
 import beauti from '../../../../images/beauti.png';
 import baby from '../../../../images/baby.png';
-export default function CatBut() {
+import Icons from '../../../Icons/Icons';
+export default function CatBut(props) {
+  const styles = StyleSheet.create({
+    CatView: {
+      width: 100,
+      height: 100,
+      borderRadius: 100 / 2,
+      shadowColor: '#dedede',
+      shadowOffset: {width: 2, height: 2},
+      shadowOpacity: 0.5,
+      shadowRadius: 5,
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+
   return (
     <View style={{width: '90%', marginTop: 50, marginLeft: '5%'}}>
       <View style={{flexDirection: 'row', marginTop: 20}}>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '5%'}]}>
+        <TouchableOpacity
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '5%',
+              borderColor: props.isFood === true && 'black',
+              borderWidth: props.isFood === true && 2,
+              opacity: props.isFood === true ? 0.5 : 1,
+            },
+          ]}
+          onPress={() => props.setisFood(!props.isFood)}>
           <Image source={food} style={{width: 50, height: 50}} />
+          {props.isFood === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '20%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisCoffee(!props.isCoffee)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '20%',
+              borderColor: props.isCoffee === true && 'black',
+              borderWidth: props.isCoffee === true && 2,
+              opacity: props.isCoffee === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={coffee} style={{width: 50, height: 50}} />
+          {props.isCoffee === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
       </View>
+
+      {/* 222222222222222 */}
+
       <View style={{flexDirection: 'row', marginTop: 50}}>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '5%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisBeauti(!props.isBeauti)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '5%',
+              borderColor: props.isBeauti === true && 'black',
+              borderWidth: props.isBeauti === true && 2,
+              opacity: props.isBeauti === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={beauti} style={{width: 50, height: 50}} />
+          {props.isBeauti === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '20%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisFashion(!props.isFashion)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '20%',
+              borderColor: props.isFashion === true && 'black',
+              borderWidth: props.isFashion === true && 2,
+              opacity: props.isFashion === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={fashion} style={{width: 50, height: 50}} />
+          {props.isFashion === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
       </View>
+
       <View style={{flexDirection: 'row', marginTop: 50}}>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '5%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisGym(!props.isGym)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '5%',
+              borderColor: props.isGym === true && 'black',
+              borderWidth: props.isGym === true && 2,
+              opacity: props.isGym === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={gym} style={{width: 50, height: 50}} />
+          {props.isGym === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '20%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisTravel(!props.isTravel)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '20%',
+              borderColor: props.isTravel === true && 'black',
+              borderWidth: props.isTravel === true && 2,
+              opacity: props.isTravel === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={travel} style={{width: 50, height: 50}} />
+          {props.isTravel === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
       </View>
+
       <View style={{flexDirection: 'row', marginTop: 50}}>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '5%'}]}>
+        <TouchableOpacity
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '5%',
+              borderColor: props.isbaby === true && 'black',
+              borderWidth: props.isbaby === true && 2,
+              opacity: props.isbaby === true ? 0.5 : 1,
+            },
+          ]}
+          onPress={() => props.setisbaby(!props.isbaby)}>
           <Image source={baby} style={{width: 50, height: 50}} />
+          {props.isbaby === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '20%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisPet(!props.isPet)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '20%',
+              borderColor: props.isPet === true && 'black',
+              borderWidth: props.isPet === true && 2,
+              opacity: props.isPet === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={pet} style={{width: 50, height: 50}} />
+          {props.isPet === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
       </View>
+
       <View style={{flexDirection: 'row', marginTop: 50}}>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '5%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisGame(!props.isGame)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '5%',
+              borderColor: props.isGame === true && 'black',
+              borderWidth: props.isGame === true && 2,
+              opacity: props.isGame === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={game} style={{width: 50, height: 50}} />
+          {props.isGame === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '20%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisWrite(!props.isWrite)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '20%',
+              borderColor: props.isWrite === true && 'black',
+              borderWidth: props.isWrite === true && 2,
+              opacity: props.isWrite === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={write} style={{width: 50, height: 50}} />
+          {props.isWrite === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
       </View>
+
       <View style={{flexDirection: 'row', marginTop: 50}}>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '5%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisLove(!props.isLove)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '5%',
+              borderColor: props.isLove === true && 'black',
+              borderWidth: props.isLove === true && 2,
+              opacity: props.isLove === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={love} style={{width: 50, height: 50}} />
+          {props.isLove === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.CatView, {marginLeft: '20%'}]}>
+        <TouchableOpacity
+          onPress={() => props.setisMusic(!props.isMusic)}
+          style={[
+            styles.CatView,
+            {
+              marginLeft: '20%',
+              borderColor: props.isMusic === true && 'black',
+              borderWidth: props.isMusic === true && 2,
+              opacity: props.isMusic === true ? 0.5 : 1,
+            },
+          ]}>
           <Image source={music} style={{width: 50, height: 50}} />
+          {props.isMusic === true && (
+            <Icons.AntDesign
+              name="checkcircle"
+              size={30}
+              color="black"
+              style={{position: 'absolute', zIndex: 10, top: 75, left: 70}}
+            />
+          )}
         </TouchableOpacity>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  CatView: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
-    shadowColor: '#dedede',
-    shadowOffset: {width: 2, height: 2},
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
